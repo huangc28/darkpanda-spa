6ae395c678f7f13a2aa523de32544a3e38ebc394
Chi-Han Huang <huangchihan@gmail.com>
2021-09-26 18:03:11 +0800

    Update npm scripts.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,9 +3,13 @@ module.exports = {
     {
       name: 'app',
       script: './server.js',
+      node_args: '-r dotenv/config',
       env_production: {
         NODE_ENV: 'production',
-      }
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
     }
   ],
 
